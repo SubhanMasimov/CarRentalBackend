87b8b3471485273b396419fe73f78b8d4ed351d7
Subhan Masimov <subhanmasimov02@gmail.com>
2021-03-15 07:36:39 +0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            //GetAll
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"{car.Id} - {car.DailyPrice} - {car.Description}");
+            }
+
+            //Id = 2
+            var CarWithId = carManager.GetById(2);
+            Console.WriteLine($"{CarWithId.Id} - {CarWithId.DailyPrice} - {CarWithId.Description}");
+
+            //Delete
+            var deleteToCar = new Car { Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 10, ModelYear = 2012, Description = "Auto Diesel" };
+            carManager.Delete(deleteToCar);
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"{car.Id} - {car.DailyPrice} - {car.Description}");
+            }
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 10, ModelYear = 2012, Description="Auto Diesel"},
+                 new Car{Id = 2, BrandId = 1, ColorId = 2, DailyPrice = 17, ModelYear = 2015, Description="Manual Diesel"},
+                  new Car{Id = 3, BrandId = 2, ColorId = 2, DailyPrice = 15, ModelYear = 2011, Description="Auto Diesel"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
